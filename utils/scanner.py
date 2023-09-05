fa87b2bbe91fdefb3eb20bd79b0735fd9ebc74a7
Noah <noah_822@163.com>
2023-09-05 09:48:48 +0800

    alter param list order

--- utils/scanner.py
+++ utils/scanner.py
@@ -1,14 +1,15 @@
 from .lazyio import LazyFileReader
 from typing import (
     Dict, List,
-    Tuple
+    Tuple,
+    Union
 )
 import spacy
 
 class Scanner:
     def __init__(self,
-                 vocab: str='en_core_web_sm',
-                 ignore_list: List[str]=None):
+                 ignore_list: List[str]=None,
+                 vocab: str='en_core_web_sm'):
         self.ignore_list = ignore_list
         self.NLP = spacy.load(vocab)
     
@@ -21,8 +22,15 @@ class Scanner:
 
     # return 
     def count_pos(self,
-                  file_loader: LazyFileReader
+                  file: Union[LazyFileReader, str],
                 ) -> Tuple[Dict[str, List[int]], int]:
+        if isinstance(file, str):
+            file_loader = LazyFileReader(file)
+        elif isinstance(file, LazyFileReader):
+            file_loader = file
+        else:
+            raise NotImplementedError('Only support str or LazyFileReader type as input')
+        
         res = {}
         pos_idx = 0
         for chunk in file_loader:
